84af3ffef0ef7021277d659e9a41888f5b397dc3
张韬 <@172.16.176.15>
2020-12-21 22:18:36 +0800

    注册成功

--- C91-S2-XM/src/com/yc/biz/UserBiz.java
+++ C91-S2-XM/src/com/yc/biz/UserBiz.java
@@ -11,21 +11,21 @@ public class UserBiz {
 	private UserDao udao=new UserDao();
 	public void register(User user) throws BizException {
 		//业务判断：同名判断，非空判断
-//		if(user.getUname()==null||user.getUname().isEmpty()) {
-//			throw new BizException("请填写您的用户名");
-//		}
-//		if(user.getUpwd()==null||user.getUpwd().isEmpty()) {
-//			throw new BizException("请填写您的密码");
-//		}
-//		if(user.getEmail()==null||user.getEmail().isEmpty()) {
-//			throw new BizException("请填写您的电子邮箱");
-//		}
-//		if(user.getAddress()==null||user.getAddress().isEmpty()) {
-//			throw new BizException("请填写您的收获地址");
-//		}
-//		if(user.getPhone()==null||user.getPhone().isEmpty()) {
-//			throw new BizException("请填写您的电话");
-//		}
+		if(user.getUname()==null||user.getUname().isEmpty()) {
+			throw new BizException("请填写您的用户名");
+		}
+		if(user.getUpwd()==null||user.getUpwd().isEmpty()) {
+			throw new BizException("请填写您的密码");
+		}
+		if(user.getEmail()==null||user.getEmail().isEmpty()) {
+			throw new BizException("请填写您的电子邮箱");
+		}
+		if(user.getAddress()==null||user.getAddress().isEmpty()) {
+			throw new BizException("请填写您的收获地址");
+		}
+		if(user.getPhone()==null||user.getPhone().isEmpty()) {
+			throw new BizException("请填写您的电话");
+		}
 		
 		
 		
@@ -35,7 +35,7 @@ public class UserBiz {
 			if(cnt>0) {
 				throw new BizException("该用户已被注册！请您重新输入用户名");
 			}
-			udao.insert(user);
+			
 			udao.insert(user);
 		} catch (SQLException e) {
 			//业务异常的信息是给用户查看的  异常转型==》异常链
