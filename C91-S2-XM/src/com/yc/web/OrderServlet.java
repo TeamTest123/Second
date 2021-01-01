60f661673f09bfc92c16d901bede24c55634415d
张韬 <@LAPTOP-JNR7VHPH>
2021-01-01 17:28:40 +0800

    2

--- C91-S2-XM/src/com/yc/web/OrderServlet.java
+++ C91-S2-XM/src/com/yc/web/OrderServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
-import com.yc.bean.Category;
 import com.yc.bean.Orders;
 import com.yc.dao.OrdersDao;
 
